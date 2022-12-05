9e0eebdc6ae9527f035a03c220651d42ca0a996f
Juju on sbire Volant <julienbailly08@gmail.com>
2022-12-05 19:09:08 +0100

    ferry is alive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use ferris_says::say;
+use std::io;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let mut first = true;
+    loop {
+        if first {
+            println!("Que va dire Ferry ?");
+            first = false;
+        } else {
+            println!("Et maintenant que va-t-il dire ?");
+        }
+
+        let mut texte = String::new();
+
+        io::stdin()
+            .read_line(&mut texte)
+            .expect("Failed to read line");
+
+        let width = texte.bytes().count();
+
+        let mut writer = BufWriter::new(stdout());
+        say(&texte, width, &mut writer).unwrap();
+        println!();
+    }
 }
